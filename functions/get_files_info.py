ecdbcd2deeb0fbb6013f9583b572436b00dff1ec
jmeldrum <jamiewmeldrum@gmail.com>
2025-09-15 13:34:37 +0100

    Added function to get file content and some testing

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,6 +3,8 @@ import os
 from os import listdir
 from os.path import isfile, isdir, join
 
+from config import MAX_CHARS
+
 def build_response_string(directory_object):
     if isfile(directory_object) or isdir(directory_object):
         return f"{directory_object.split("/")[-1]}: file_size={os.path.getsize(directory_object)} bytes, is_dir={not isfile(directory_object)}"
@@ -22,10 +24,31 @@ def get_files_info(working_directory, directory="."):
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         
         contents = list(map(lambda x: f"{absolute_path}/{x}", listdir(absolute_path)))
-        try:
-            return "\n".join(list(map(build_response_string, contents)))
-        except Exception as e:
-            print(e)
-            return("Unexpected directory object")
-    except:
-        return "An unexpected error has occured"
+        return "\n".join(list(map(build_response_string, contents)))
+    
+    except Exception as e:
+        print(e)
+        return "Error: An unexpected error has occured"
+    
+
+def get_file_content(working_directory, file_path):
+
+    try:
+        absolute_path = os.path.abspath(os.path.join(working_directory, file_path))
+        if not absolute_path.startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        
+        if not isfile(absolute_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        
+        with open(absolute_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+
+            if len(file_content_string) == MAX_CHARS:
+                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+            return file_content_string
+
+    except Exception as e:
+        print(e)
+        return "Error: An unexpected error has occured"
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_files_info, get_file_content
 
 class TestGetFileInfo(unittest.TestCase):
 
@@ -10,26 +10,38 @@ class TestGetFileInfo(unittest.TestCase):
         self.assertTrue("tests.py: file_size=" in result)
         self.assertTrue("pkg: file_size=" in result)
 
-        print(result)
-
     def test_get_pkg(self):
         result = get_files_info("calculator", "pkg")
         self.assertTrue("calculator.py: file_size=" in result)
         self.assertTrue("render.py: file_size" in result)
 
-        print(result)
-
     def test_get_bin(self):
         result = get_files_info("calculator", "/bin")
         self.assertEqual(result, "Error: Cannot list \"/bin\" as it is outside the permitted working directory")
-
-        print(result)
     
     def test_get_outside_working_dir(self):
         result = get_files_info("calculator", "../")
         self.assertEqual(result, "Error: Cannot list \"../\" as it is outside the permitted working directory")
 
+    def test_get_get_file_calculator_main(self):
+        result = get_file_content("calculator", "main.py")
         print(result)
+        self.assertTrue("def main():" in result)
+
+    def test_get_get_file_calculator_pkg_calculator(self):
+        result = get_file_content("calculator", "pkg/calculator.py")
+        print(result)
+        self.assertTrue(result.startswith("class Calculator:"))
+
+    def test_get_get_file_calculator_bin_cat(self):
+        result = get_file_content("calculator", "/bin/cat")
+        print(result)
+        self.assertEqual(result, "Error: Cannot read \"/bin/cat\" as it is outside the permitted working directory")
+
+    def test_get_get_file_calculator_does_not_exist(self):
+        result = get_file_content("calculator", "pkg/does_not_exist.py")
+        print(result)
+        self.assertEqual(result, "Error: File not found or is not a regular file: \"pkg/does_not_exist.py\"")
 
 if __name__ == "__main__":
     unittest.main()
