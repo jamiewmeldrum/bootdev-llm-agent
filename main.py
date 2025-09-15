0ccdfa9d96f704b147f519c7e8d1e222f6dc9da9
jmeldrum <jamiewmeldrum@gmail.com>
2025-09-15 11:35:26 +0100

    Updated to begin supporting converstations rather than fire and forget

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 import sys
+
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -13,10 +15,14 @@ def main():
         exit(1)
 
     query = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=query)]),
+    ]
+
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
-        contents=query
+        contents=messages
         )
     
     print(response.text)
