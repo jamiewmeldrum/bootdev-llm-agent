e0487addfdf3ecc6e96810d62892cb42bb82cad2
jmeldrum <jamiewmeldrum@gmail.com>
2025-09-15 11:32:26 +0100

    Updted to allow user defined query

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,10 +7,16 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 def main():
+
+    if len(sys.argv) != 2:
+        print("Must provide a command line query")
+        exit(1)
+
+    query = sys.argv[1]
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=query
         )
     
     print(response.text)
