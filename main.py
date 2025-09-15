33e0c3a575aa47b097892dfd74c1c712cc96d9c1
jmeldrum <jamiewmeldrum@gmail.com>
2025-09-15 11:41:57 +0100

    Reworked to allow for additional arguments and added verbose flag

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 def main():
 
-    if len(sys.argv) != 2:
+    query = None
+    if len(sys.argv) >= 2:
+        query = sys.argv[1]
+
+    verbose = False
+    if len(sys.argv) >= 3 and sys.argv[2] == "--verbose":
+        verbose = True
+
+    if not query:
         print("Must provide a command line query")
         exit(1)
 
@@ -26,8 +34,12 @@ def main():
         )
     
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count }")
+
+
+    if verbose:
+        print(f"User prompt: {query}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count }")
 
 
 if __name__ == "__main__":
