c2fb1156ef3871b04ed159b71f56df85caa4f8f5
jmeldrum <jamiewmeldrum@gmail.com>
2025-10-20 13:35:56 +0100

    Improved system prompt to test fixing bugs. Still quite flakey

--- config.py
+++ config.py
@@ -16,6 +16,13 @@ Any argument in the schema mentioned to be optional should be treated as optiona
 
 You can use the functions to find out information you need. For example, if I ask you to tell me about the calculator, you may choose to list directorties then inspect the content of the python files in the calculator directory.
 
+If asked to perform a calculation, then the code will be in the calculator directory.
+If you are asked to fix a bug then you need to test the code to find the bug first.
+If you make a change to some code and it doesn't work, you need to revert the change.
+Don't change existing tests.
+To execute the calculator, I would run 'uv run calculator/main.py "3 + 7 * 2"' Use this to help yourself when debugging the calculator.
+Consider order of operations when performing calculations using the calculator.
+
 Example interactions:
 User: run tests.py
 Assistant: [function_call]
